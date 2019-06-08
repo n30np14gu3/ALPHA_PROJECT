64008cdf7e0098ee4bf683d37a78885534252026
ShockByte <darknetsoft@yandex.ru>
2019-06-08 18:38:33 +0300

    [ADD] * JSON LIB * XNET LIB * Manual Map Injector

--- Loader .NET/MainForm.cs
+++ Loader .NET/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Loader.NET
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
